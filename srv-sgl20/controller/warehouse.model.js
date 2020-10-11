1ba3d6b2e40578c0cc54b4b0c3ef16fc3d9d5eb2
devNica <designsdevnicaragua@gmail.com>
2020-10-11 09:41:52 -0600

    update-enable-warehouse

--- srv-sgl20/controller/warehouse.model.js
+++ srv-sgl20/controller/warehouse.model.js
@@ -38,4 +38,51 @@ Warehouse.create = (req, res)=>{
     
 }
 
+Warehouse.update = (req, res)=>{
+    if(req.method === 'GET'){
+        ws.findAll({where: {idwarehouse: req.params.id }})
+        .then(result=>{
+           res.render('templates/edit-warehouse',{warehouse: result})
+        })
+        .catch(error => console.log(error));
+
+    }
+    if(req.method === 'POST'){
+        
+        ws.update({ warehouse: req.body.warehouse }, {
+            where: {
+              idwarehouse: req.params.id
+            }
+        })
+        .then(result =>{
+            res.redirect('/sisgal/api/v1.0/warehouse-list')
+        })
+        .catch(error => console.log(error))
+    }
+}
+
+Warehouse.disable = (req, res)=>{
+    ws.update({ isActive: 0 }, {
+        where: {
+          idwarehouse: req.params.id
+        }
+    })
+    .then(result =>{
+        res.redirect('/sisgal/api/v1.0/warehouse-list')
+    })
+    .catch(error => console.log(error))
+}
+
+Warehouse.enable = (req, res)=>{
+    ws.update({ isActive: 1 }, {
+        where: {
+          idwarehouse: req.params.id
+        }
+    })
+    .then(result =>{
+        res.redirect('/sisgal/api/v1.0/warehouse-list')
+    })
+    .catch(error => console.log(error))
+}
+
 module.exports = Warehouse;
--- srv-sgl20/routes/warehouse.routes.js
+++ srv-sgl20/routes/warehouse.routes.js
@@ -4,6 +4,9 @@ let Warehouse = require('../controller/warehouse.model');
 router.get('/warehouse-list', Warehouse.fetchAll)
 router.get('/create-warehouse', Warehouse.create)
 router.post('/create-warehouse', Warehouse.create)
-
+router.get('/edit-warehouse/:id', Warehouse.update)
+router.post('/edit-warehouse/:id', Warehouse.update)
+router.get('/disable-warehouse/:id', Warehouse.disable)
+router.get('/enable-warehouse/:id', Warehouse.enable)
 
 module.exports = router;
